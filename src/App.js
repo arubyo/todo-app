2dffcf3691389c739f8b122e0b54d69138fe3701
Alyssa O'Friel <alyssa.ofriel@q1.com.au>
2022-02-08 16:51:58 +1100

    inital commit, project set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import styledComponents from 'styled-components';
+import { Form, FormLabel, Button, InputGroup, FloatingLabel } from 'react-bootstrap';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+         ToDo
+        
+       
       </header>
+
+      <div className="container">
+        <Form>
+          <FloatingLabel>
+            Something that needs to be done?
+          </FloatingLabel>
+
+        </Form>
+
+      </div>
     </div>
   );
 }
