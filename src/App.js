1591bfa504ce9cfd856f6f6afb6123111adb0930
Alyssa O'Friel <alyssa.ofriel@q1.com.au>
2022-02-09 11:00:08 +1100

    add basic structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import './App.css';
-import styledComponents from 'styled-components';
-import { Form, FormLabel, Button, InputGroup, FloatingLabel } from 'react-bootstrap';
+import styled from 'styled-components';
+import { Form, InputGroup } from 'react-bootstrap';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+
+const StyledHeader = styled.header `
+  font-size: 3.5em;
+  text-align: center;
+  transform: var(--transform);
+  color: var(--color, palevioletred);
+`;
+
+const Button = styled.button`
+  color: papayawhip;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  background: palevioletred;
+  font-weight: bold;
+  `;
+
+  const ResetButton = styled(Button)`
+    color: palevioletred;
+    background: papayawhip;
+  
+  `;
+
 
 function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
+      <StyledHeader className="App-header">
       
          ToDo
         
        
-      </header>
+      </StyledHeader>
 
       <div className="container">
         <Form>
-          <FloatingLabel>
-            Something that needs to be done?
-          </FloatingLabel>
+          <Form.Group className="mb-3" controlId="formBasicToDoList">
+            <FloatingLabel controlId="floatingTextarea" label="Comments" className="mb-3">
+              <Form.Control as="textarea" placeholder="Leave a comment here" id="new-todo" />
+            </FloatingLabel>
+            <ResetButton className="btn m-2" variant="outline-secondary" as="input" type="reset" value="Reset" />{' '}
+            <Button className="btn">Add</Button>
+          </Form.Group>
+          
 
         </Form>
 
